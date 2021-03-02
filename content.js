de1b67fd2b1883174f82831c83d9c57b3f6733a6
Philippe Sultan <philippe.sultan@gmail.com>
2021-03-02 17:58:55 +0100

    Remove ontrack listener.
    the 'ontrack' listener is not needed since we can match MediaTracks from
    the RTCPeerConnections and those in the <video /> DOM elements by
    comparing JavaScript references.

--- content.js
+++ content.js
@@ -10,52 +10,7 @@ const inject =
       constructor(configuration) {
         super(configuration);
 
-        this._mediaStreamIds = [];
-
         window._webrtc_getstats.peerConnections.push(this);
-
-        /**
-         * DOM <video/> elements play audio/video tracks from local devices
-         * (getUserMedia) or from remote peers (RTCPeerConnection.addTrack()).
-         * To do do, a MediaStream is set as the 'srcObject' of the <video/>
-         * element.
-         *
-         * A MediaStream has a unique identifier (id), and is composed of audio
-         * and video tracks.
-         *
-         * A track event indicates that a new track has been added to this
-         * RTCPeerConnection instance, resulting in audio/video flowing in.
-         * Since getRemoteStreams() is being deprecated, we add an object method
-         * that retrieves the streams this track is part of so that we can have
-         * a match between RTCPeerConnections and DOM <video/> elements.
-         */
-        this.addEventListener(
-          "track",
-          (e) => {
-            if (!e.streams || e.streams.length < 1) {
-              return;
-            }
-
-            if (this.iceConnectionState !== "completed") {
-              // Track added but PeerConnection is not up
-              return;
-            }
-
-            // We should get an array of a single element
-            this._mediaStreamIds = e.streams.map((elem) => {
-              return elem.id;
-            });
-            console.log(
-              "[content.js] this._mediaStreamIds :",
-              this._mediaStreamIds
-            );
-          },
-          false
-        );
-      }
-
-      getMediaStreamIds() {
-        return this._mediaStreamIds;
       }
     }
 
