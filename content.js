07b6dcbbd13c335bab01447995328056b9f369b9
Philippe Sultan <philippe.sultan@gmail.com>
2021-03-04 12:29:43 +0100

    Return early

--- content.js
+++ content.js
@@ -39,74 +39,81 @@ const loopGetStats = () => {
   }
 
   window._webrtc_getstats.peerConnections.forEach((pc) => {
-    if (pc.iceConnectionState === "completed") {
-      pc.getReceivers().forEach((receiver) => {
-        if (receiver?.track) {
-          console.log(`[${receiver.track.kind}][Receiver] stats ------`);
-          receiver.getStats().then((stats) => {
-            stats.forEach((stat) => {
-              stat.type == "candidate-pair" &&
-                stat.nominated &&
-                console.log(
-                  "stat.currentRoundTripTime :",
-                  stat.currentRoundTripTime
-                );
-            });
-          });
-
-          const element = findDOMElementForTrack(receiver.track);
-          const container = document.createElement("div");
-          container.className = _dom_prefix + "-container";
-          container.innerText = "Audio";
-
-          if (
-            element &&
-            !element.parentNode.querySelector("." + container.className)
-          ) {
-            element.parentNode.insertBefore(container, element);
-          }
-
-          console.log(`[${receiver.track.kind}][Receiver] element :`, element);
-        }
-      });
-
-      pc.getSenders().forEach((sender) => {
-        console.log("sender :", sender);
-
-        if (sender?.track) {
-          console.log(`[${sender.track.kind}][Sender] stats ------`);
-          console.log(
-            `[${sender.track.kind}][Sender] sender.track :`,
-            sender.track
-          );
-          sender.getStats().then((stats) => {
-            console.log(`[${sender.track.kind}][Sender] stats :`, stats);
-            stats.forEach((stat) => {
-              stat.type == "candidate-pair" &&
-                stat.nominated &&
-                console.log(
-                  "stat.currentRoundTripTime :",
-                  stat.currentRoundTripTime
-                );
-            });
-          });
-
-          const element = findDOMElementForTrack(sender.track);
-          const container = document.createElement("div");
-          container.className = _dom_prefix + "-container";
-          container.innerText = "Video";
-
-          if (
-            element &&
-            !element.parentNode.querySelector("." + container.className)
-          ) {
-            element.parentNode.insertBefore(container, element);
-          }
-
-          console.log(`[${sender.track.kind}][Sender] element :`, element);
-        }
-      });
+    if (pc.iceConnectionState !== "completed") {
+      return;
     }
+
+    pc.getReceivers().forEach((receiver) => {
+      if (!receiver?.track) {
+        // No RTCRtpReceiver or MediaTrack, return
+        return;
+      }
+
+      console.log(`[${receiver.track.kind}][Receiver] stats ------`);
+      receiver.getStats().then((stats) => {
+        stats.forEach((stat) => {
+          stat.type == "candidate-pair" &&
+            stat.nominated &&
+            console.log(
+              "stat.currentRoundTripTime :",
+              stat.currentRoundTripTime
+            );
+        });
+      });
+
+      const element = findDOMElementForTrack(receiver.track);
+      const container = document.createElement("div");
+      container.className = _dom_prefix + "-container";
+      container.innerText = "Audio";
+
+      if (
+        element &&
+        !element.parentNode.querySelector("." + container.className)
+      ) {
+        element.parentNode.insertBefore(container, element);
+      }
+
+      console.log(`[${receiver.track.kind}][Receiver] element :`, element);
+    });
+
+    pc.getSenders().forEach((sender) => {
+      console.log("sender :", sender);
+      if (!sender?.track) {
+        // No RTCRtpSender or MediaTrack, return
+        return;
+      }
+
+      console.log(`[${sender.track.kind}][Sender] stats ------`);
+      console.log(
+        `[${sender.track.kind}][Sender] sender.track :`,
+        sender.track
+      );
+      sender.getStats().then((stats) => {
+        console.log(`[${sender.track.kind}][Sender] stats :`, stats);
+        stats.forEach((stat) => {
+          stat.type == "candidate-pair" &&
+            stat.nominated &&
+            console.log(
+              "stat.currentRoundTripTime :",
+              stat.currentRoundTripTime
+            );
+        });
+      });
+
+      const element = findDOMElementForTrack(sender.track);
+      const container = document.createElement("div");
+      container.className = _dom_prefix + "-container";
+      container.innerText = "Video";
+
+      if (
+        element &&
+        !element.parentNode.querySelector("." + container.className)
+      ) {
+        element.parentNode.insertBefore(container, element);
+      }
+
+      console.log(`[${sender.track.kind}][Sender] element :`, element);
+    });
   });
 
   setTimeout(loopGetStats, interval * 1000);
