39465495343a4ca24b1a463cbd0021a7e0d80e94
Philippe Sultan <philippe.sultan@gmail.com>
2021-09-13 22:06:28 +0200

    Compute values for outbound A/V streams.
    We now call getStats on RTCRtpSender only.

--- content.js
+++ content.js
@@ -50,7 +50,7 @@ const loopGetStats = () => {
     /**
      * 'transporter' contains either a RTCRtpReceiver or a RTCRtpSender
      */
-    for (const transporter of [...pc.getReceivers(), ...pc.getSenders()]) {
+    for (const transporter of [...pc.getSenders()]) {
       if (!transporter?.track) {
         // No RTCRtpReceiver/RTCRtpSender or MediaTrack, return
         break;
@@ -85,12 +85,17 @@ const loopGetStats = () => {
          */
         window._webrtc_getstats.transporterStats[element.srcObject.id] = {
           type: transporter.constructor.name,
-          track: transporter.track,
           stats: {
-            audio: {},
-            video: {},
-          },
-        };
+            rtt: 0,
+            bytesSent: 0,
+            bitrate: 0,
+            availableOutgoingBitrate: 0,
+            audio: {
+            },
+            video: {
+            },
+          }
+        }
       }
 
       try {
@@ -104,14 +109,82 @@ const loopGetStats = () => {
 
         stats.forEach((stat) => {
           switch (stat.type) {
+            case "remote-inbound-rtp": {
+              const outboundRTPReport = stats.get(stat.localId)
+              if (stat.kind === 'video' && outboundRTPReport?.frameHeight) {
+                console.log('outboundRTPReport :', outboundRTPReport)
+                console.log('trackStats.video :', trackStats.video)
+                console.log('outboundRTPReport.frameHeight :', outboundRTPReport.frameHeight)
+                console.log('outboundRTPReport.frameWidth :', outboundRTPReport.frameWidth)
+
+                const reportVideoIndex = `${outboundRTPReport.frameWidth}x${outboundRTPReport.frameHeight}`
+
+                if (!trackStats.video[reportVideoIndex]) {
+                  trackStats.video[reportVideoIndex] = {
+                    framesSent: 0,
+                    frameRate: 0,
+                    packetsSent: 0,
+                    packetsLost: 0,
+                    instantPacketLossPercent: 0,
+                    fractionLost: 0,
+                    jitter: 0,
+                    bytesSent: 0,
+                    bitrate: 0,
+                  }
+                }
+
+                const diffFramesSent = outboundRTPReport.framesSent-trackStats.video[reportVideoIndex].framesSent
+                trackStats.video[reportVideoIndex].frameRate=diffFramesSent/interval
+                trackStats.video[reportVideoIndex].framesSent = outboundRTPReport.framesSent
+
+                const diffPacketsSent = outboundRTPReport.packetsSent-trackStats.video[reportVideoIndex].packetsSent
+                trackStats.video[reportVideoIndex].packetsSent = outboundRTPReport.packetsSent
+                const diffPacketsLost = stat.packetsLost-trackStats.video[reportVideoIndex].packetsLost
+
+                trackStats.video[reportVideoIndex].packetsLost = stat.packetsLost
+                trackStats.video[reportVideoIndex].instantPacketLossPercent = 100*diffPacketsLost/diffPacketsSent
+                trackStats.video[reportVideoIndex].fractionLost = stat.fractionLost
+
+                trackStats.video[reportVideoIndex].jitter = stat.jitter
+                trackStats.video[reportVideoIndex].roundTripTime = stat.roundTripTime
+
+                const diffBytesSent = outboundRTPReport.bytesSent-trackStats.video[reportVideoIndex].bytesSent
+                trackStats.video[reportVideoIndex].bitrate = diffBytesSent*8/interval
+                trackStats.video[reportVideoIndex].bytesSent = outboundRTPReport.bytesSent
+              } else if (stat.kind === 'audio') {
+                const diffPacketsSent = outboundRTPReport.packetsSent-trackStats.audio.packetsSent
+                trackStats.audio.packetsSent = outboundRTPReport.packetsSent
+                const diffPacketsLost = stat.packetsLost-trackStats.audio.packetsLost
+                trackStats.audio.packetsLost = stat.packetsLost
+                trackStats.audio.instantPacketLossPercent = 100*diffPacketsLost/diffPacketsSent
+                trackStats.audio.fractionLost = stat.fractionLost
+
+                trackStats.audio.jitter = stat.jitter
+                trackStats.audio.roundTripTime = stat.roundTripTime
+                const diffBytesSent = outboundRTPReport.bytesSent-trackStats.audio.bytesSent
+                trackStats.audio.bitrate = diffBytesSent*8/interval
+                trackStats.audio.bytesSent = outboundRTPReport.bytesSent
+              }
+
+              break;
+            }
             case "candidate-pair": {
               console.log(
                 `[${transporter.track.kind}][${transporter.constructor.name}][${stat.type}] :`,
                 stat
               );
-              if (stat.nominated) {
-                trackStats[transporter.track.kind] = stat.currentRoundTripTime;
-                container.innerText = trackStats[transporter.track.kind];
+              if (stat.nominated && transporter.track.kind === 'audio') {
+                // The values found in the 'candidate-pair' report are the
+                // same in both 'audio' and 'video' tracks. If we want to
+                // compute the bitrate, we must extract and store the
+                // 'bytesSent' out of one of the two kinds
+                console.log('stat.bytesSent :', stat.bytesSent)
+                console.log('trackStats.bytesSent :', trackStats.bytesSent)
+                trackStats.rtt = stat.currentRoundTripTime
+                const diffBytesSent = stat.bytesSent-trackStats.bytesSent
+                trackStats.bitrate = diffBytesSent*8/interval
+                trackStats.bytesSent = stat.bytesSent
+                trackStats.availableOutgoingBitrate = stat.availableOutgoingBitrate
               }
               break;
             }
